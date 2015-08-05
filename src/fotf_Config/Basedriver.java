a99a14902cb486e9930e9998387797a0623483bc
HemanathanKumar <kumar.hemanathan@gmail.com>
2015-08-05 19:27:11 +0530

    Update Basedriver.java

--- src/fotf_Config/Basedriver.java
+++ src/fotf_Config/Basedriver.java
@@ -1,13 +1,17 @@
 package fotf_Config;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerConfigurationException;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
